1955c2a9342d05b7950285a4035c6f4b0868d64c
Deividas-Git <ltdeividas.lt@gmail.com>
2026-02-23 21:15:42 +0200

    created new appbar for root screen and improved medication screen where added medications now are loaded

--- lib/screens/medication/medication_screen.dart
+++ lib/screens/medication/medication_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vaistu_priminimo_sistema/models/medication/user_medication.dart';
 import 'package:vaistu_priminimo_sistema/providers/user_provider.dart';
 import 'package:vaistu_priminimo_sistema/screens/medication/add_medication/add_type_selection_screen.dart';
+import 'package:vaistu_priminimo_sistema/widgets/root_app_bar.dart';
+import 'package:vaistu_priminimo_sistema/widgets/themed_container_widget.dart';
 
 class MedicationScreen extends StatefulWidget {
   const MedicationScreen({super.key});
@@ -11,7 +14,7 @@ class MedicationScreen extends StatefulWidget {
 }
 
 class _MedicationScreenState extends State<MedicationScreen> {
-  final TextEditingController _textEditingController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
 
   void _onAddMedication() {
     Navigator.push(
@@ -22,7 +25,7 @@ class _MedicationScreenState extends State<MedicationScreen> {
 
   @override
   void dispose() {
-    _textEditingController.dispose();
+    _searchController.dispose();
     super.dispose();
   }
 
@@ -30,10 +33,39 @@ class _MedicationScreenState extends State<MedicationScreen> {
   @override
   Widget build(BuildContext context) {
     final user = context.watch<UserProvider>().appUser;
+    final medications = user?.userMedications;
     debugPrint("MEDICATION SKAICIUS: ${user?.userMedications.length}");
     return Scaffold(
+      appBar: RootAppBar(title: "Mano vaistai"),
       body: SafeArea(
-        child: Padding(padding: const EdgeInsets.all(12.0), child: Column()),
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: medications!.isEmpty
+              ? _NoMedicationsAddedNoticeWidget()
+              : SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                          hintText: "Pridėto vaisto paieška",
+                        ),
+                      ),
+                      //SizedBox(height: 10),
+                      ...List.generate(
+                        medications.length,
+                        (int index) => MedicationTile(
+                          medication: medications.elementAt(index),
+                        ),
+                      ),
+                      SizedBox(height: 55),
+                    ],
+                  ),
+                ),
+        ),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -43,6 +75,79 @@ class _MedicationScreenState extends State<MedicationScreen> {
           child: Icon(Icons.add, color: Colors.white),
         ),
       ),
+    );
+  }
+}
+
+class _NoMedicationsAddedNoticeWidget extends StatelessWidget {
+  const _NoMedicationsAddedNoticeWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.medication_outlined, size: 60, color: Colors.grey),
+          SizedBox(height: 16),
+          Text("Neturite pridėtų vaistų", style: TextStyle(fontSize: 18)),
+        ],
+      ),
+    );
+  }
+}
+
+class MedicationTile extends StatelessWidget {
+  const MedicationTile({super.key, required this.medication});
+
+  final UserMedication medication;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Divider(),
+        ThemedContainerWidget(
+          doesHeightExpand: true,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    medication.name!,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    medication.medicationType!.getLabel,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  if (medication.currentQuantity != null)
+                    Text(
+                      "Likutis: ${medication.medicationType!.consumedAmoutIsInteger ? medication.currentQuantity!.toInt() : medication.currentQuantity} ${medication.medicationType!.getUnit}",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: ColorScheme.of(context).primary,
+                  ),
+                  child: SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
