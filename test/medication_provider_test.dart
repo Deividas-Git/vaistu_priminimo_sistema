173742313188f3727495b03539389c2f0bf166be
Deividas-Git <ltdeividas.lt@gmail.com>
2026-04-21 00:53:22 +0300

    added tests for agenda service

--- test/medication_provider_test.dart
+++ test/medication_provider_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -16,14 +18,32 @@ void main() {
     provider = MedicationProvider(service);
   });
 
+  tearDown(() {
+    provider.stopListening();
+  });
+
+  test('initial state', () {
+    expect(provider.uerMedications, isEmpty);
+  });
+
   test('addMedication calls service', () async {
-    final med = UserMedication();
+    final med = UserMedication(id: '1', name: 'Test Med');
 
     when(() => service.addMedication(med, 'uid1')).thenAnswer((_) async {});
 
     await provider.addMedication(med, 'uid1');
 
     verify(() => service.addMedication(med, 'uid1')).called(1);
+  });
+
+  test('updateMedication calls service', () async {
+    final med = UserMedication(id: '1', name: 'Test Med');
+
+    when(() => service.updateMedication(med, 'uid1')).thenAnswer((_) async {});
+
+    await provider.updateMedication(med, 'uid1');
+
+    verify(() => service.updateMedication(med, 'uid1')).called(1);
   });
 
   test('removeMedication calls service', () async {
@@ -34,5 +54,27 @@ void main() {
     await provider.removeMedication('med1', 'uid1');
 
     verify(() => service.removeMedication('med1', 'uid1')).called(1);
+  });
+
+  test('getMedicationFromRegistrationCode calls service', () async {
+    final med = UserMedication(name: 'Test Med');
+    when(
+      () => service.getMedicationFromRegistrationCode('123'),
+    ).thenAnswer((_) async => med);
+
+    final result = await provider.getMedicationFromRegistrationCode('123');
+
+    expect(result, med);
+    verify(() => service.getMedicationFromRegistrationCode('123')).called(1);
+  });
+
+  test('stopListening cancels subscription and clears list', () {
+    provider.stopListening();
+  });
+
+  test('getMedicationFromId returns correct medication', () {
+    final med1 = UserMedication(id: '1', name: 'Med1');
+    final med2 = UserMedication(id: '2', name: 'Med2');
+    expect(() => provider.getMedicationFromId('1'), throwsStateError);
   });
 }
